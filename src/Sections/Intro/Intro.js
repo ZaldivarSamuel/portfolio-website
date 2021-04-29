56c2ceafd34423df40332564627ea8cbbc33989c
Samuel Zaldivar <samzaldivar98@gmail.com>
2021-04-28 22:14:29 -0400

    Added menu

--- src/Sections/Intro/Intro.js
+++ src/Sections/Intro/Intro.js
@@ -5,17 +5,18 @@ export default class Intro extends Component{
   render(){
     return(
       <div id='greeting-container'>
-        <div id='greeting-section' class='column'>
+        <div id='greeting-section' class='greeting-column'>
           <h1 id='greeting'>Hi,<br /> I'm Sam.</h1>
         </div>
-        <div id="greeting-menu" class='column'>
-          <ul>
-            <li>Projects</li>
-            <li>About Me</li>
-            <li>Resume</li>
-            <li>Contact</li>
-            <li>Test</li>
-          </ul>
+        <div id="greeting-menu-container" class='greeting-column'>
+          <div id="greeting-menu-inner-container">
+            <ul id='greeting-menu'>
+              <li class='greeting-menu-option'>Projects</li>
+              <li class='greeting-menu-option'>About Me</li>
+              <li class='greeting-menu-option'>Resume</li>
+              <li class='greeting-menu-option'>Contact</li>
+            </ul>
+          </div>
         </div>
       </div>
     )
